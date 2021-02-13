7f448a1062c0bacdcad30ed056e225e75ce4c6e5
Remyavineeth <dhruvhome@vineeths-MacBook-Pro.local>
2021-02-13 14:22:18 -0600

    initial commit

---
+++ src/main/java/com/demo/helloworlddemo/HelloworlddemoApplication.java
@@ -0,0 +1,20 @@
+package com.demo.helloworlddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworlddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworlddemoApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
